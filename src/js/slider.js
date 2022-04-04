d55fb11e33295637bb6933a80237ba07065f9802
BogdanMyagkiy <myagkiybogdan@gmail.com>
2022-04-04 14:17:43 +0300

    bug-fix-slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,27 +1,22 @@
-/* Индекс слайда по умолчанию */
-let slideIndex = 1;
+var slideIndex = 1;
 showSlides(slideIndex);
 
-/* Функция увеличивает индекс на 1, показывает следующй слайд*/
 function plusSlide() {
   showSlides((slideIndex += 1));
 }
 
-/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
 function minusSlide() {
   showSlides((slideIndex -= 1));
 }
 
-/* Устанавливает текущий слайд */
 function currentSlide(n) {
   showSlides((slideIndex = n));
 }
 
-/* Основная функция слайдера */
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName('review-slider__item');
-  let dots = document.getElementsByClassName('review-slider-dots__item');
+  var i;
+  var slides = document.getElementsByClassName('review-slider__item');
+  var dots = document.getElementsByClassName('review-slider-dots__item');
   if (n > slides.length) {
     slideIndex = 1;
   }
